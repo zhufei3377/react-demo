2af07e611a1af22aa1ca8517db5893c7d51a91ad
zhufei3377 <1076164088@qq.com>
2022-11-27 17:24:46 +0800

    add mock server

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './Footer.css';
 
-export default function Footer({ copyright = "© Copyright 2017 ThoughtWorks" }) {
+export default function Footer({ copyright = "© Copyright 2022 ZhuFei" }) {
   return (
     <footer className="c-wrapper--footer">
       <p>{copyright}</p>
